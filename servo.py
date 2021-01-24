3d34c7f8b3f08091ccfa2b19bae25a6e9e01fd23
avi9360 <savinash406@gmail.com>
2021-01-24 17:18:11 +0530

    3

--- servo.py
+++ servo.py
@@ -23,11 +23,13 @@ def SetAngle2(angle):
     GPIO.output(11,True)
     pwm.ChangeDutyCycle(duty)
     GPIO.output(11, False)
-
-if __name__=='__main__':
-    p1 = Process(target=SetAngle1(150))
-    p1.start()
-    p2 = Process(target=SetAngle2(150))
-    p2.start()
+def a():
+    print("dsafdasf")
+def Main(ang):
+    if __name__=='__main__':
+        p1 = Process(target=SetAngle1(ang))
+        p1.start()
+        p2 = Process(target=SetAngle2(ang))
+        p2.start()
 pwm.stop()
 GPIO.cleanup()
---
+++ test.py
@@ -0,0 +1,2 @@
+def a():
+    print('asdfdsaf')
